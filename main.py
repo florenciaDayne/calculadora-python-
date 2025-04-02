17ef9c1477c0dcd56da69b2f12bfa317b60cd26d
florenciaDayne <florenciadayne@gmail.com>
2025-04-01 22:21:50 -0300

    calculadora

---
+++ main.py
@@ -0,0 +1,17 @@
+#Crea una Calculadora
+
+num1= float(input("numero 1: "))
+num2= float(input("numero 2: "))
+operacion= input("introduce una operacion (+ - * /): ")
+
+match operacion:
+    case "+":
+        res= num1+num2
+    case "-":
+        res= num1-num2
+    case "*":
+        res= num1*num2
+    case "/":
+        res= num1/num2
+
+print(f"El resultado de {num1} {operacion} {num2} = {res}")
